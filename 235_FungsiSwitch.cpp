dd659a16ab20a9343aee1bedb1fb49cad68a2fc0
harismuhammad0912 <muhammadharis091204@gmail.com>
2024-03-26 14:49:34 +0700

    fungsiSwitch1

---
+++ 235_FungsiSwitch.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+using namespace std;
+
+int bilangan1,bilangan2;
+
+void inputdata()
+{
+    cout <<"masukan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "masukan bilangan kedua : ";
+    cin >> bilangan2;
+}
+
+
+    int penjualan (int a, int b)
+{
+    return a + b;
+}
+
